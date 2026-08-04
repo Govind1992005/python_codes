7b36b001e0d61e206b05971785328127a47cb88d
govind <kanugulapawan143@gmail.com>
2026-08-04 06:41:49 +0000

    first commit

---
+++ sum.py
@@ -0,0 +1,6 @@
+
+print("To add two numbers.")
+number1=int(input("Enter the first number : "))
+number2=int(input("Enter the second number : "))
+sum=number1+number2
+print("The sum of ", number1 , " and ", number2 , "is ", sum)
